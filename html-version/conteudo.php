0f38fad94cdd00b2e78f6d1861122d50b5209257
pgfarias <85649773+pgfarias@users.noreply.github.com>
2026-07-07 15:16:06 -0300

    Update conteudo.php

--- html-version/conteudo.php
+++ html-version/conteudo.php
@@ -17,10 +17,10 @@ require __DIR__ . '/includes/header.php';
       // Adicionamos o nome do arquivo .php como o 5º índice (posição [5]) de cada linha:
       ['Mensagem','28 Set 2025','E sereis minhas testemunhas','O poder do Espírito Santo enviado para nos alinhar ao verdadeiro foco da igreja de Cristo.','preaching.jpg', 'msg-sereis-minhas-testemunhas.php'],
       ['Devocional','24 Set 2025','Obediência gera relacionamento','Deus nos exorta a crescer e florescer com esperança, mesmo em terra de sofrimento e dor.','worship-band.jpg', 'msg-obediencia-gera-relacionamento.php'],
-      ['Esboço','18 Set 2025','Famílias que servem juntas','O legado da fé atravessa o tempo.','family.jpg', 'msg-familias-que-servem.php'],
-      ['Mensagem','11 Set 2025','O caminho da bonança','A bondade prática como marca do povo de Deus.','community-1.jpg', 'msg-caminho-da-bonanca.php'],
-      ['Devocional','07 Set 2025','A mente renovada','Transformar-se pela renovação do entendimento.','preaching.jpg', 'msg-mente-renovada.php'],
-      ['Estudo','02 Set 2025','Fundamentos da fé','Uma trilha para novos convertidos.','youth.jpg', 'msg-fundamentos-da-fe.php'],
+      ['Mensagem', '18 Set 2025', 'Decisões', 'Quais decisões você tomou essa semana? Uma reflexão sobre as escolhas de Davi e o impacto de colocar a fé no Deus vivo.', 'family.jpg', 'msg-decisoes.php'],
+    <!--  ['Mensagem','11 Set 2025','O caminho da bonança','A bondade prática como marca do povo de Deus.','community-1.jpg', 'msg-caminho-da-bonanca.php'], -->
+    <!--  ['Devocional','07 Set 2025','A mente renovada','Transformar-se pela renovação do entendimento.','preaching.jpg', 'msg-mente-renovada.php'], -->
+    <!-- ['Estudo','02 Set 2025','Fundamentos da fé','Uma trilha para novos convertidos.','youth.jpg', 'msg-fundamentos-da-fe.php'], -->
     ] as $m): ?>
       <article class="card-soft article-card">
         <div class="thumb"><img src="assets/img/<?= e($m[4]) ?>" alt="" loading="lazy"></div>
